67e83d67ffe685374b4de9e778bc11ab393cb29e
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-06-12 12:50:43 -0300

    Initial Commit

---
+++ PlistReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlistReader
+//
+//  Created by Cast on 6/12/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlistReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PlistReader
+//
+//  Created by Cast on 6/12/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
